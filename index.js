e684b185656535933b20a1d062e8cba3ddd591b9
Rafael Santos <rafael@back4app.com>
2016-09-19 13:15:07 -0300

    correction if(module) and remove exit(1)

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
   if (require.main === module) {
-    var path = require('path');
-    var configFilePath = process.argv[2];
-    var config = {};
-
-    if (configFilePath) {
-      configFilePath = path.resolve(configFilePath);
-      try {
-        config = require(configFilePath);
-      } catch(e) {
-        console.log('Cannot load '+configFilePath);
-        process.exit(1);
+      module.exports = function (config) {
+          if (config) {
+              config.exportedModule = true;
+          }
+          return require('./lib')(config);
       }
-    }
-    var utils = require('./lib')(config);
   } else {
-    module.exports = function (config) {
-      if (config) {
-        config.exportedModule = true;
+      var path = require('path');
+      var configFilePath = process.argv[2];
+      var config = {};
+
+      if (configFilePath) {
+          configFilePath = path.resolve(configFilePath);
+          try {
+              config = require(configFilePath);
+          } catch(e) {
+              console.log('Cannot load '+configFilePath);
+              process.exit(1);
+          }
       }
-      return require('./lib')(config);
-    }
+      var utils = require('./lib')(config);
   }
