e0fdd49f242903f1d36add3009abb9bb7b95c665
Rafael Santos <rafael@back4app.com>
2016-09-19 16:36:48 -0300

    correction index

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-  if (require.main === module) {
-      module.exports = function (config) {
-          if (config) {
-              config.exportedModule = true;
-          }
-          return require('./lib')(config);
-      }
-  } else {
-      var path = require('path');
-      var configFilePath = process.argv[2];
-      var config = {};
+if (require.main === module) {
+    var path = require('path');
+    var configFilePath = process.argv[2];
+    var config = {};
 
-      if (configFilePath) {
-          configFilePath = path.resolve(configFilePath);
-          try {
-              config = require(configFilePath);
-          } catch(e) {
-              console.log('Cannot load '+configFilePath);
-              process.exit(1);
-          }
-      }
-      var utils = require('./lib')(config);
-  }
+    config.exportedModule = false;
+    if (configFilePath) {
+        configFilePath = path.resolve(configFilePath);
+        try {
+            config = require(configFilePath);
+        } catch(e) {
+            console.log('Cannot load '+configFilePath);
+            process.exit(1);
+        }
+    }
+    var utils = require('./lib')(config);
+} else {
+    module.exports = function (config) {
+        if (config) {
+            config.exportedModule = true;
+        }
+        return require('./lib')(config);
+    }
+}
